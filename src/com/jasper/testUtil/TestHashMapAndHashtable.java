42ce73b7d8b36bb30c179a8e6cecf4c4bef19161
JasperZXY <jasperzxy@gmail.com>
2014-10-19 17:25:14 +0800

    add

---
+++ src/com/jasper/testUtil/TestHashMapAndHashtable.java
@@ -0,0 +1,22 @@
+package com.jasper.testUtil;
+
+import java.util.HashMap;
+import java.util.Hashtable;
+
+public class TestHashMapAndHashtable {
+	public static void main(String[] args) {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		map.put("1", "1");
+		map.put("2", null);
+		map.put(null, null);
+		System.out.println(map);
+		
+		Hashtable<String, Object> table = new Hashtable<String, Object>();
+		table.put("1", "1");
+//		table.put("2", null);  //会抛空指针异常
+//		table.put(null, null); //会抛空指针异常
+		System.out.println(table);
+		
+	}
+
+}
