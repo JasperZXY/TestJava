427a0ffb1bc7bcc4d3cdb92946a021e27d1f1a4a
Jasper in xd <Jasperzxy@gamil.com>
2016-01-09 12:31:55 +0800

    'JDK8一些例子'

---
+++ src/com/jasper/JDK8/Lambda.java
@@ -0,0 +1,47 @@
+package com.jasper.JDK8;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class Lambda {
+	public static void main(String[] args) {
+		//旧版本排序
+		List<String> names = Arrays.asList("peter", "anna", "mike", "xenia");
+		Collections.sort(names, new Comparator<String>() {
+		    @Override
+		    public int compare(String a, String b) {
+		        return b.compareTo(a);
+		    }
+		});
+		System.out.println(names);
+		
+		//新版本排序
+		names = Arrays.asList("peter", "anna", "mike", "xenia");
+//		Collections.sort(names, (String a, String b) -> {
+//		    return b.compareTo(a);
+//		});
+		Collections.sort(names, (a, b) -> b.compareTo(a));
+		System.out.println(names);
+		
+		new Thread(() -> {
+			for (int i=0; i<10; i++) {
+				System.out.println(Thread.currentThread().getName() + ":" + i);
+				try {
+					Thread.sleep(100);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+		
+		try {
+			Thread.sleep(1500);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
