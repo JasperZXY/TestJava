31d2771f24182655ca18ce4deff76cb01840fbac
Jasper in xd <Jasperzxy@gamil.com>
2016-04-02 15:23:29 +0800

    -

--- src/com/jasper/io/Test.java
+++ src/com/jasper/io/Test.java
@@ -5,9 +5,14 @@ import java.io.BufferedWriter;
 import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.OutputStreamWriter;
+import java.net.URL;
 
 public class Test {
 	public static void main(String[] args) throws Exception {
+		URL url = Test.class.getClassLoader().getResource("a.txt");
+		System.out.println(url.getPath());
+		System.out.println(url.getFile());
+		System.out.println("===========");
 		BufferedReader br = new BufferedReader(new FileReader(System.getProperty("user.dir") + "/src/networkips_city_original.txt"));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(System.getProperty("user.dir") + "/src/networkips_city.txt")));
 		String line = null;
