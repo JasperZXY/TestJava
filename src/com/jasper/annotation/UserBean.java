cc9f57abbb15cf8031fe060ccbb56c4f7bdbf4c1
Jasper in xd <Jasperzxy@gamil.com>
2016-04-23 17:22:56 +0800

    -

---
+++ src/com/jasper/annotation/UserBean.java
@@ -0,0 +1,19 @@
+package com.jasper.annotation;
+
+/**
+ * 用户实体bean
+ * @author Jasper
+ *
+ */
+@Table(name="user")
+public class UserBean {
+	@Column(name="uid")
+	int id;
+	
+	@Column(name="user_name", type="varchar(30)")
+	String name;
+	
+	@Column
+	String address;
+	
+}
---
+++ src/com/jasper/io/TestFile.java
@@ -0,0 +1,26 @@
+package com.jasper.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+public class TestFile {
+	public static void main(String[] args) {
+		File file = new File("file/a.txt");
+		System.out.println("exists:" + file.exists());
+		System.out.println("getPath:" + file.getPath());
+		System.out.println("getAbsolutePath:" + file.getAbsolutePath());
+		System.out.println("toURI:" + file.toURI().toString());
+		try {
+			System.out.println("toURL:" + file.toURL().toString());
+		} catch (MalformedURLException e1) {
+			e1.printStackTrace();
+		}
+		try {
+			System.out.println("getCanonicalPath:" + file.getCanonicalPath());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
