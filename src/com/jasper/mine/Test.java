2b05a8ff2f9d032354ae5238f1f00ffee65ee542
Jasper <Jasper>
2016-06-16 18:48:51 +0800

    ignore intellij 生成的文件与文件夹

--- src/com/jasper/mine/Test.java
+++ src/com/jasper/mine/Test.java
@@ -4,10 +4,11 @@ import java.util.Arrays;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println(Arrays.toString("😃".getBytes()));
+		System.out.println(Arrays.toString("😃❤".getBytes()));
 		System.out.println(Long.MAX_VALUE);
 		System.out.println(Double.MAX_VALUE);
 		System.out.println(1e1);
 	}
-
+	
 }
+
