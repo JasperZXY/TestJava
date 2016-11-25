c2a0d7de629cb2816a514d044af823fd70748f3a
JasperZXY <Jasperzxy@gamil.com>
2016-11-25 18:04:14 +0800

    -

--- src/com/jasper/testThread/testClass/ExchangerTest.java
+++ src/com/jasper/testThread/testClass/ExchangerTest.java
@@ -1,9 +1,12 @@
-package com.jasper.testThread;
+package com.jasper.testThread.testClass;
 
 import java.util.concurrent.Exchanger;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * Exchanger可以在两个线程之间交换数据，只能是2个线程，他不支持更多的线程之间互换数据
+ */
 public class ExchangerTest {
 
 	public static void main(String[] args) {
@@ -14,10 +17,10 @@ public class ExchangerTest {
 			public void run() {
 				try {
 
-					String data1 = "zxx";
+					String data1 = "10元";
 					System.out.println("线程" + Thread.currentThread().getName()
 							+ "正在把数据" + data1 + "换出去");
-					Thread.sleep((long) (Math.random() * 10000));
+					Thread.sleep((long) (Math.random() * 3000));
 					String data2 = (String) exchanger.exchange(data1);
 					System.out.println("线程" + Thread.currentThread().getName()
 							+ "换回的数据为" + data2);
@@ -31,10 +34,10 @@ public class ExchangerTest {
 			public void run() {
 				try {
 
-					String data1 = "lhm";
+					String data1 = "盒饭";
 					System.out.println("线程" + Thread.currentThread().getName()
 							+ "正在把数据" + data1 + "换出去");
-					Thread.sleep((long) (Math.random() * 10000));
+					Thread.sleep((long) (Math.random() * 3000));
 					String data2 = (String) exchanger.exchange(data1);
 					System.out.println("线程" + Thread.currentThread().getName()
 							+ "换回的数据为" + data2);
@@ -43,5 +46,6 @@ public class ExchangerTest {
 				}
 			}
 		});
+		service.shutdown();
 	}
 }
