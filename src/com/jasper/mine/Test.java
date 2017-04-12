7d756ab8bf65015dda7e0fd20a2d97888a082fa7
Jasper Zhong <JasperZXY@gmail.com>
2017-04-12 23:52:04 +0800

    -

--- src/com/jasper/mine/Test.java
+++ src/com/jasper/mine/Test.java
@@ -1,10 +1,30 @@
 package com.jasper.mine;
 
 
+import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Test {
     public static void main(String[] args) {
 //        System.out.println(test());
-        System.out.println(Runtime.getRuntime().availableProcessors());
+//        System.out.println(Runtime.getRuntime().availableProcessors());
+//        BigDecimal bigDecimal1 = new BigDecimal(19.501);
+//        System.out.println(bigDecimal1.setScale(2, BigDecimal.ROUND_HALF_UP));
+//        BigDecimal bigDecimal2 = new BigDecimal(19.506);
+//        System.out.println(bigDecimal2.setScale(2, BigDecimal.ROUND_HALF_UP));
+
+        try {
+            Date rel = parse("2016-02-13", "yyyy/MM/dd") ;
+        } catch (ParseException e){
+            e.printStackTrace();
+        }
+    }
+
+    private static Date parse(String dateStr, String pattern) throws ParseException {
+        SimpleDateFormat format = new SimpleDateFormat(pattern);
+        return format.parse(dateStr);
     }
 
     public static String test() {
