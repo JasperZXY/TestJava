c5a0dc810223bdc34d3febc43132dd54256a351f
Jasper Zhong <JasperZXY@gmail.com>
2018-02-25 15:28:21 +0800

    -

--- src/com/jasper/mine/Test.java
+++ src/com/jasper/mine/Test.java
@@ -2,18 +2,24 @@ package com.jasper.mine;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println(m().data);            // 跑通了
-        System.out.println(m().data.getClass()); // 抛异常
-    }
-    static Result<UserInfo> m() {
-        Result result = new Result();
-        result.data = new Result();
-        return result;
-    }
-    static class Result<T> {
-        public T data;
-    }
-    static class UserInfo {
+        System.out.println("123");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0; i<10; i++) {
+                    System.out.println(Thread.currentThread().getName() + "i");
+                }
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i=0; i<10; i++) {
+                    System.out.println(Thread.currentThread().getName() + "i");
+                }
+            }
+        }).start();
     }
 }
 
