557a947307633693d73978ac6259d65fa6eeddbc
zhongxianyao <jasperzxy@gmail.com>
2018-06-23 22:15:53 +0800

    测试

--- src/com/zxy/mine/Test.java
+++ src/com/zxy/mine/Test.java
@@ -2,24 +2,7 @@ package com.zxy.mine;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("123");
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i=0; i<10; i++) {
-                    System.out.println(Thread.currentThread().getName() + "i");
-                }
-            }
-        }).start();
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                for (int i=0; i<10; i++) {
-                    System.out.println(Thread.currentThread().getName() + "i");
-                }
-            }
-        }).start();
+        System.out.println("Hello");
     }
 }
 
