230130001770addb74dd34c84dd4b50282109e62
zhongxianyao <zhongxianyao@youzan.com>
2018-06-26 16:37:32 +0800

    -

--- src/com/jasper/other/test.java
+++ src/com/jasper/other/test.java
@@ -1,25 +1,32 @@
 package com.jasper.other;
 
 public class test {
-	private String baseName = "base";
 
-	public test() {
-		callName();
-	}
+    private String baseName = "base";
 
-	public void callName() {
-		System.out.println("test=" + baseName);
-	}
 
-	static class Sub extends test {
-		private String baseName = "sub";
+    public test() {
+        callName();
+    }
 
-		public void callName() {
-			System.out.println("sub=" + baseName);
-		}
-	}
 
-	public static void main(String[] args) {
-		test bb = new Sub();
-	}
+    public void callName() {
+        System.out.println("test=" + baseName);
+    }
+
+
+    static class Sub extends test {
+
+        private String baseName = "sub";
+
+
+        public void callName() {
+            System.out.println("sub=" + baseName);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(Long.parseLong("55D2C83", 16));
+    }
 }
