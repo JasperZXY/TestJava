a6397a384a28a3621109358318854ee5ed7cfaad
zhongxianyao <jasperzxy@gmail.com>
2019-06-15 17:22:05 +0800

    加注释

--- src/com/zxy/testKeyword/testStatic/TestStaticOrder.java
+++ src/com/zxy/testKeyword/testStatic/TestStaticOrder.java
@@ -3,6 +3,15 @@ package com.zxy.testKeyword.testStatic;
 /**
  * static语句,构造代码块,构造方法的执行顺序
  * @author Jasper
+ *
+ * 输出结果
+ * 静态初始化语句块1
+ * 静态方法m1
+ * 静态初始化语句块2
+ * main start...
+ * 普通方法m2
+ * 构造方法
+ * main end...
  */
 public class TestStaticOrder {
 	static {
@@ -13,9 +22,11 @@ public class TestStaticOrder {
 	static {
 		System.out.println("静态初始化语句块2");
 	}
+
 	public TestStaticOrder() {
 		System.out.println("构造方法");
 	}
+
 	public static int m1() {
 		System.out.println("静态方法m1");
 		return 100;
@@ -24,6 +35,7 @@ public class TestStaticOrder {
 		System.out.println("普通方法m2");
 		return 100;
 	}
+
 	public static void main(String[] args) {
 		System.out.println("main start...");
 		new TestStaticOrder();
